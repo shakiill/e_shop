c2814f8c2612cc2177b7ee212dc400e055d0eb95
shakiill <Khadizashakil@1543>
2022-12-27 22:06:53 +0600

    url config

---
+++ apps/product/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('list/', views.OrderDetailsListView.as_view(), name='order_details_list'),
+]
--- e_shop/urls.py
+++ e_shop/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="index.html"), name='home'),
+    path('product/', include('apps.product.urls')),
 ]
 
 if settings.DEBUG:
